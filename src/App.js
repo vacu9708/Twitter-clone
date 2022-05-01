db3cdd60211714745030755392199f789d35af9f
Youngsik Yang <vacu9708@gmail.com>
2022-05-02 02:57:16 +0900

    Start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hiiiiii</h1>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
